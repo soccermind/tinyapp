b219ea21ffeef3a2da1f03a375b8854add14bcaf
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-11-11 22:48:04 -0500

    Add user-specific permissions to access urls

--- express_server.js
+++ express_server.js
@@ -9,12 +9,13 @@ app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-// const urlDatabase = { }
+// const urlDatabase = { };
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "7yrp7d" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "7yrp7d" }
 };
 const users = { };
+// const users = { id: { id: '7yrp7d', email: 'email@email.com', password: 'pass1' } };
 
 const fetchUser = function(id) {
   return users[id];
@@ -26,41 +27,98 @@ const findUserByEmail = function(email) {
     // console.log("User in findUserByEmail: ", user);
     // console.log('user.email: ', users[user].email, 'email:', email);
     if (users[key].email === email) {
-      return users[key];
+      // return users[key];
+      return fetchUser(key);
     } 
   }
   return null; 
+};
+
+// returns an array with the URLs that belong to the currently logged-in user
+const urlsForUser = function(id) {
+  // const userUrls = [];
+  const filteredDatabase = { };
+  for (url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      filteredDatabase[url] = urlDatabase[url];
+    }
+  }
+  return filteredDatabase;
+};
+
+const isUserUrl = function (url, id) {
+  console.log("isUserUrl url:", url, "id", id);
+  console.log("urlDatabase[url]['userID']", urlDatabase[url]['userID']);
+  if (urlDatabase[url]['userID'] === id) {
+    return true;
+  }
+  return false;
 };
 
 function generateRandomString() {
   return Math.random().toString(36).substring(2,8);
 }
 
+// URLS ROUTES
 app.get("/urls", (req, res) => {
   const userObj = fetchUser(req.cookies['user_id']);
   console.log('UserObj= ', userObj);
-  const templateVars = { user: userObj, urls: urlDatabase };
+  filteredUrls = urlsForUser(req.cookies['user_id']);
+  const templateVars = { user: userObj, urls: filteredUrls };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log('req.body line 132', req.body);
+  const shortURL = generateRandomString();
+  console.log("urlDatabase[shortURL]", urlDatabase[shortURL])
+  urlDatabase[shortURL] = { longURL: req.body['longURL'], userID: req.cookies['user_id'] }
+  console.log(urlDatabase);
+  // This redirect should be replaced?
+  res.redirect(`/urls/${shortURL}`);
 });
 
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL; // added .longURL
   console.log(longURL);
   console.log(res.statusCode);
   res.redirect(longURL);
 });
 
+// CREATE NEW ROUTE
 app.get("/urls/new", (req, res) => {
   const userObj = fetchUser(req.cookies['user_id']);
+  if (!userObj) {
+    return res.redirect("/login");
+  }
   const templateVars = { user: userObj }
   res.render("urls_new", templateVars);
 });
 
+
+
+// EDIT ROUTES 
 app.get("/urls/:shortURL", (req, res) => {
   const userObj = fetchUser(req.cookies['user_id']);
-  const templateVars = { user: userObj, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  const templateVars = { user: userObj, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL }; // added . longURL
+    if(!userObj) {
+      return res.send("Cannot access url. Log in required!");
+    } else if (!isUserUrl(templateVars.shortURL, req.cookies['user_id'])) { 
+      return res.send("Cannot access url. It does not belong to this user.")
+    } else {
+      res.render("urls_show", templateVars);
+    }
+});
+
+app.post("/urls/:id", (req, res) => {
+  if (!isUserUrl(req.params.id, req.cookies['user_id'])) {
+    return res.send("Cannot modify url!!!\n");
+  }
+  console.log('req.body line 141', req.body);
+  console.log("urlDatabase[req.params.id]", urlDatabase[req.params.id])
+  urlDatabase[req.params.id].longURL = req.body['longURL']; // added .longURL
+  res.redirect("/urls");
 });
 
 // REGISTER ROUTES
@@ -90,7 +148,6 @@ app.post("/register", (req, res) => {
   }
 });
 
-
 // LOGIN ROUTES
 app.get("/login", (req, res) => {
   const templateVars = { user: null };
@@ -111,6 +168,7 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+// Useless routes
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -123,25 +181,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  // res.send("Ok");
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body['longURL'];
-  console.log(urlDatabase);
-  res.redirect(`/urls/${shortURL}`);
-});
-
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body['longURL'];
-  res.redirect("/urls");
-});
-
+// DELETE ROUTE
 app.post("/urls/:shortURL/delete", (req, res) => {
+  if (!isUserUrl(req.params.shortURL, req.cookies['user_id'])) {
+    return res.send("Cannot delete url!!!\n");
+  }
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
+// LOGOUT ROUTE
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id');
   res.redirect("/urls");
